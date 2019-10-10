b9d5994c0b58c26ab14e78ec9c2d191b5035ff2a
richelbilderbeek <richel@richelbilderbeek.nl>
2019-10-10 14:01:02 +0200

    One test less is skipped (although it is unknown if it really works)

--- R/DownloadSearchedTaxa.R
+++ R/DownloadSearchedTaxa.R
@@ -5,32 +5,45 @@ APItaxon <- function(taxon) {
 }
 
 
-MatchTaxatoEOLID <- function(ListOfTaxa, exact=TRUE, ...){ 
+MatchTaxatoEOLID <- function(ListOfTaxa, exact = TRUE){
   #Match a search taxon to an EOLID for downloading or storing
-  #API can support fuzzy matching up to 30 matches if exact=F, but then pages have to be specified.  Might be a good thing to add later. 
+  #API can support fuzzy matching up to 30 matches if exact=F, but then pages have to be specified.  Might be a good thing to add later.
   eolPageNumbers <- rep(NA, length(ListOfTaxa))
   speciesNameForRef <- rep(NA, length(ListOfTaxa))
-  for (i in sequence(length(ListOfTaxa))) {  
+  for (i in sequence(length(ListOfTaxa))) {
     taxon <- APItaxon(ListOfTaxa[i])
-	web <- paste('http://eol.org/api/search/1.0.xml?q=', taxon, '&exact=', exact, '&page=1', sep="")
-    a <- getURL(web, ...)
+	  web <- paste('https://eol.org/api/search/1.0.xml?q=', taxon, '&exact=', exact, '&page=1', sep="")
+    a <- getURL(web)
     searchRes <- NULL
-    searchRes <- xmlToList(xmlRoot(xmlParse(a, getDTD=FALSE), ...), simplify=FALSE)
-    if(searchRes$totalResults == 1) {  #didn't match any eol taxa
-      eolPageNumbers[i] <- searchRes$entry$id  #there are other matches sometimes as well
-      speciesNameForRef[i] <- searchRes$entry$title
+    searchRes <- xmlToList(xmlRoot(xmlParse(a, getDTD=FALSE)), simplify=FALSE)
+    if(searchRes$totalResults$text == 1) {  #didn't match any eol taxa
+      eolPageNumbers[i] <- searchRes$result$result$id$text  #there are other matches sometimes as well
+      speciesNameForRef[i] <- searchRes$result$result$title
     }
   }
-  return(data.frame(ListOfTaxa, speciesNameForRef, eolPageNumbers, stringsAsFactors=F))
+  return(
+    data.frame(
+      ListOfTaxa,
+      speciesNameForRef,
+      eolPageNumbers,
+      stringsAsFactors = FALSE
+    )
+  )
 }
 
 
-DownloadSearchedTaxa <- function(ListOfTaxa, to.file=TRUE, MyKey=NULL, exact=TRUE, verbose=TRUE, ...) {
-  matches <- MatchTaxatoEOLID(ListOfTaxa, exact=exact, ...)
+DownloadSearchedTaxa <- function(
+  ListOfTaxa,
+  to.file=TRUE,
+  MyKey=NULL,
+  exact=TRUE,
+  verbose=TRUE
+) {
+  matches <- MatchTaxatoEOLID(ListOfTaxa, exact = exact)
   pagesToDownload <- unique(matches[,3])
-  fileNames <- data.frame(paste("eol", matches[,3], ".xml", sep=""), stringsAsFactors=FALSE)
+  fileNames <- data.frame(paste0("eol", matches[,3], ".xml"), stringsAsFactors=FALSE)
   colnames(fileNames) <- "fileNames"
   if(any(!is.na(pagesToDownload))) {
-    DownloadEOLpages(as.numeric(pagesToDownload), to.file, MyKey, verbose=verbose, ...)  
+    DownloadEOLpages(as.numeric(pagesToDownload), to.file, MyKey, verbose=verbose)
   }
 }
--- inst/tests/testAPI1.R
+++ inst/tests/testAPI1.R
@@ -1,6 +1,5 @@
 test_that("API unchanged", {
-  skip("Not now")
-  MyTaxa <- c("Camelus bactrianus", "Camelus dromedarius", "Hippopotamus amphibius", "Rattus rattus", "Rana cascadae", "Bufo bufo")
-  testEOLs <- DownloadSearchedTaxa(MyTaxa, to.file=FALSE)
+  ListOfTaxa <- c("Camelus bactrianus", "Camelus dromedarius", "Hippopotamus amphibius", "Rattus rattus", "Rana cascadae", "Bufo bufo")
+  testEOLs <- DownloadSearchedTaxa(ListOfTaxa = ListOfTaxa, to.file = FALSE)
   is_identical_to(testEOLs, MyEOLs)
 })
